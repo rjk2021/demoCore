d72077c841c4ad45f195910c0e2924775ffdac48
Rajiv_Crm <rajiv@crm-masters.com>
2021-09-11 11:18:59 +0100

    Repo and Entity

--- Infrastructure/Data/ProductRespository.cs
+++ Infrastructure/Data/ProductRespository.cs
@@ -19,13 +19,17 @@ namespace Infrastructure.Data
 
         public async Task<Product> GetProductByIdAsync(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _context.Products. 
+            Include(p=>p.ProductBrand).
+            Include(p=>p.ProductType).FirstOrDefaultAsync(p=>p.Id==id);
         }
 
         public async Task<List<Product>> GetProductsAsync()
         {
 
-            return await _context.Products.ToListAsync();
+            return await _context.Products.
+            Include(p=>p.ProductBrand).Include(p=>p.ProductType)
+            .ToListAsync();
         }
         public async Task<List<ProductType>> GetProductTypesAsync()
         {
