0e9ccbd593a35da9de2cffe1009697cd3fff4ab1
Rajiv_Crm <rajiv@crm-masters.com>
2021-11-01 10:16:30 +0000

    01Nov2021

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
 import { BrowserModule } from '@angular/platform-browser';
 
 
@@ -9,6 +9,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 
 import { HomeModule } from './home/home.module';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 
 
 @NgModule({
@@ -25,7 +26,10 @@ import { HomeModule } from './home/home.module';
     
     HomeModule
   ],
-  providers: [],
+  providers: [
+
+    {provide:HTTP_INTERCEPTORS,useClass:ErrorInterceptor,multi:true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { NavigationExtras, Router } from '@angular/router';
+import { catchError } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+
+  constructor(private router: Router,private toastr:ToastrService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+
+      catchError(error=>{
+
+        if(error){
+          if(error.status===404){
+           // this.router.navigateByUrl('/not-found');
+           this.toastr.error(error.error.message,error.error.statusCode);
+           
+
+          }
+          if(error.status===400){
+            // this.router.navigateByUrl('/not-found');
+
+            if(error.error.errors){
+              throw error.error;
+            }else{
+            this.toastr.error(error.error.message,error.error.statusCode);
+            }
+            
+ 
+           }
+          if(error.status===500){
+
+            const navigationExtras:NavigationExtras={state:{error:error.error}};
+
+            this.router.navigateByUrl('/server-error',navigationExtras);
+           // this.toastr.error(error.error.message,error.error.statusCode);
+          }
+        }
+        return throwError(error);
+      })
+    );
+  }
+}
